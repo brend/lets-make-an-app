58cc32d5e149c8df395d015170468b6edb3f8b44
Philipp Brendel <waldrumpus@gmail.com>
2019-07-20 13:15:08 +0200

    Initial Commit

---
+++ Calcmaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calcmaster
+//
+//  Created by Philipp Brendel on 20.07.19.
+//  Copyright © 2019 Entenwolf Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
